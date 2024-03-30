b78ffa0fd21200e650f604cebb0bf99a0466b573
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-03-30 17:54:20 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -1,56 +1,62 @@
 function solution(fees, records) {
-    const [defaultTime, defaultAmount, unitTime, unitAmount] = fees;
-    const endTime = {
-        hour: 23,
-        minute : 59
-    };
-    
-    const amountMap = new Map();
-    
-    records.forEach((value) => {
-        const [time, carNumber, type] = value.split(' ');
-        const [hour, minute] = time.split(':').map((v) => Number(v));
-        
-        if(type === "IN"){
-            if(amountMap.has(carNumber)){
-                amountMap.set(carNumber, {...amountMap.get(carNumber), entryTime:[hour, minute]});
-                return;
-            };
-            amountMap.set(carNumber, {interval:0, entryTime:[hour, minute]});
-        };
-        
-        if(type === "OUT"){
-            const [entryHour, entryMinute] = amountMap.get(carNumber).entryTime;
-            
-            const minuteInterval = minute - entryMinute;
-            const hourInterval = hour - entryHour;
-            const interval = hourInterval * 60 + minuteInterval;
-            
-            amountMap.set(carNumber, {interval:amountMap.get(carNumber).interval + interval, entryTime:null});
-        };
-    });
-    
-    const amountArr = new Array(...amountMap);
-    
-    const result = amountArr.sort().map(([key, value]) => {
-        let {interval, entryTime} = value;
-        
-        if(entryTime){
-            const [entryHour, entryMinute] = entryTime;
-            const minuteInterval = 59 - entryMinute;
-            const hourInterval = 23 - entryHour;
-            interval += hourInterval * 60 + minuteInterval;
-        }
-        
-        const overtime = interval - defaultTime;
-        let amount = defaultAmount;
+  const [defaultTime, defaultAmount, unitTime, unitAmount] = fees;
+  const endTime = {
+    hour: 23,
+    minute: 59,
+  };
 
-        if(overtime > 0){
-            amount += Math.ceil(overtime / unitTime) * unitAmount;
-        };
-        
-        return amount;
-    });
-    
-    return result; 
+  const amountMap = new Map();
+
+  records.forEach((value) => {
+    const [time, carNumber, type] = value.split(' ');
+    const [hour, minute] = time.split(':').map((v) => Number(v));
+
+    if (type === 'IN') {
+      if (amountMap.has(carNumber)) {
+        amountMap.set(carNumber, {
+          ...amountMap.get(carNumber),
+          entryTime: [hour, minute],
+        });
+        return;
+      }
+      amountMap.set(carNumber, { interval: 0, entryTime: [hour, minute] });
+    }
+
+    if (type === 'OUT') {
+      const [entryHour, entryMinute] = amountMap.get(carNumber).entryTime;
+
+      const minuteInterval = minute - entryMinute;
+      const hourInterval = hour - entryHour;
+      const interval = hourInterval * 60 + minuteInterval;
+
+      amountMap.set(carNumber, {
+        interval: amountMap.get(carNumber).interval + interval,
+        entryTime: null,
+      });
+    }
+  });
+
+  const amountArr = new Array(...amountMap);
+
+  const result = amountArr.sort().map(([key, value]) => {
+    let { interval, entryTime } = value;
+
+    if (entryTime) {
+      const [entryHour, entryMinute] = entryTime;
+      const minuteInterval = endTime.minute - entryMinute;
+      const hourInterval = endTime.hour - entryHour;
+      interval += hourInterval * 60 + minuteInterval;
+    }
+
+    const overtime = interval - defaultTime;
+    let amount = defaultAmount;
+
+    if (overtime > 0) {
+      amount += Math.ceil(overtime / unitTime) * unitAmount;
+    }
+
+    return amount;
+  });
+
+  return result;
 }
