2ad90ed0875057b67a5128fd0a8273472946d0d2
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-04-01 08:45:30 +0900

    [level 2] Title: 숫자의 표현, Time: 0.54 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,21 +1,43 @@
 function solution(n) {
-    let result = 0;
-    let sum = 1;
-    let left = 1;
-    let right = 1;
-    
-    while(left <= n/2 && right <= n/2 + 1){
-        if(sum >= n){
-            if(sum === n){
-                result++;
-            }
-            sum -= left;
-            left++;
-        }else{
-            right++;
-            sum += right;
-        };
-    };
-    
-    return result + 1;
+  let result = 0;
+  let sum = 1;
+  let left = 1;
+  let right = 1;
+
+  while (left <= n / 2 && right <= n / 2 + 1) {
+    if (sum >= n) {
+      if (sum === n) {
+        result++;
+      }
+      sum -= left;
+      left++;
+    } else {
+      right++;
+      sum += right;
+    }
+  }
+
+  return result + 1;
+}
+
+function solution2(n) {
+  let result = 0;
+  let sum = 1;
+  let left = 1;
+  let right = 1;
+
+  while (left <= n / 2 && right <= n / 2 + 1) {
+    if (sum >= n) {
+      if (sum === n) {
+        result++;
+      }
+      sum -= left;
+      left++;
+    } else {
+      right++;
+      sum += right;
+    }
+  }
+
+  return result + 1;
 }
